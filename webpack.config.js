1431585b1d004d23fcc2b02b16fd267654f3a318
Patrick Gross <bears_lair@protonmail.com>
2024-12-10 16:32:59 -0800

    fixed webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-MediaSourceHandle.exports = {
+module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
