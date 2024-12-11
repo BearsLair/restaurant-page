26455aa98cecb2316e64265674382c0774b3ebc0
Patrick Gross <bears_lair@protonmail.com>
2024-12-11 13:51:33 -0800

    added description and contact info

--- src/about.js
+++ src/about.js
@@ -3,9 +3,42 @@ const aboutPage = () => {
   mainContent.innerHTML = "";
 
   const title = document.createElement("h1");
-  const titleText = document.createTextNode("About");
+  const titleText = document.createTextNode("About us");
   title.appendChild(titleText);
   mainContent.appendChild(title);
+
+  const aboutSection = document.createElement("div");
+  mainContent.appendChild(aboutSection);
+  aboutSection.classList.add("about-section");
+
+  const about = document.createElement("p");
+  aboutSection.appendChild(about);
+  about.classList.add("about-description");
+  const aboutText = document.createTextNode(
+    "We were established in 1955, and have for the past 60 years provided our community with fine diing for the whole family! We welcome all!"
+  );
+  about.appendChild(aboutText);
+
+  const contactInfo = document.createElement("div");
+  mainContent.appendChild(contactInfo);
+  contactInfo.classList.add("contact-info");
+
+  // Add classes for styling
+  const contactTitle = document.createElement("h3");
+  contactInfo.appendChild(contactTitle);
+  const contactTitleText = document.createTextNode("Contact Us");
+  contactTitle.appendChild(contactTitleText);
+
+  const phone = document.createElement("p");
+  contactInfo.appendChild(phone);
+  const phoneText = document.createTextNode("Phone: 555-555-5555");
+  phone.appendChild(phoneText);
+  const email = document.createElement("p");
+  contactInfo.appendChild(email);
+  const emailText = document.createTextNode(
+    "Email: mrbear@totallynotfakeemail.com"
+  );
+  email.appendChild(emailText);
 };
 
 export default aboutPage;
